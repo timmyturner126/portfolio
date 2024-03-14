2e211af0064f53dc9cea07dc477e7ceb9ed8b244
Timeea Samoila <timeeasamoila12@gmail.com>
2024-03-14 21:57:22 +0100

    added card

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root "pages#index"
-  # resources :pages, only: [:home]
+  resources :pages, only: [:home]
 end
